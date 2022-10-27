937254b0a16083dea94eed03f7499e8f2bc4fb4a
adel-elmala <adel.elmala2025@gmail.com>
2022-10-27 20:02:08 +0200

    2 triangles YAY!

--- part2/createWindow/createWin.cpp
+++ part2/createWindow/createWin.cpp
@@ -14,16 +14,27 @@ void initTriangleData();
 void initVShader();
 void initFShader();
 void createShaderProg();
+void createShaderProg2();
 
+void initFShader2();
 // Globals
 
 unsigned int Triangle_VBO;
 unsigned int Triangle_EBO;
 unsigned int Triangle_VAO;
 
+unsigned int Triangle2_VBO;
+// unsigned int Triangle2_EBO;
+unsigned int Triangle2_VAO;
+
 unsigned int vShaderID;
 unsigned int fShaderID;
 unsigned int shaderProg;
+
+
+unsigned int fShader2ID;
+unsigned int shaderProg2;
+
 
 int main(int argc, const char *argv[])
 {
@@ -37,6 +48,8 @@ int main(int argc, const char *argv[])
     initVShader();
     initFShader();
     createShaderProg();
+initFShader2();
+    createShaderProg2();
 
     // render loop
     while (!glfwWindowShouldClose(window))
@@ -48,9 +61,14 @@ int main(int argc, const char *argv[])
 
         glUseProgram(shaderProg);
         glBindVertexArray(Triangle_VAO);
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
-        glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+        glUseProgram(shaderProg2);
+        glBindVertexArray(Triangle2_VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+        // glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -100,30 +118,52 @@ void processInput(GLFWwindow *window)
 
 void initTriangleData()
 {
-    // float verticies[] = {-0.5f, -0.5f, 0.0f,
-    //                      0.5f, -0.5f, 0.0f,
-    //                      0.0f, 0.5f, 0.0f};
+    float verticies[] = {1.0f, 0.0f, 0.0f, // Triangle 1 (top right)
+                         0.0f, 1.0f, 0.0f,
+                         0.0f, 0.0f, 0.0f};
 
-    float verticies[] = {0.5f, 0.5f, 0.0f,   // top right
-                         0.5f, -0.5f, 0.0f,  // bottom right
-                         -0.5f, -0.5f, 0.0f, // bottom left
-                         -0.5f, 0.5f, 0.0f}; // top left
+    float verticiesT2[] = {-1.0f, 0.0f, 0.0f, // Triangle 2 (bottom left)
+                         0.0f, -1.0f, 0.0f,
+                         0.0f, 0.0f, 0.0f};
 
-    unsigned int indices[] = {0, 1, 3,  // first triangle
-                              1, 2, 3}; // second triangle
+    // float verticies[] = {0.5f, 0.5f, 0.0f,   // top right
+    //                      0.5f, -0.5f, 0.0f,  // bottom right
+    //                      -0.5f, -0.5f, 0.0f, // bottom left
+    //                      -0.5f, 0.5f, 0.0f}; // top left
 
+    // unsigned int indices[] = {0, 1, 3,  // first triangle
+    //                           1, 2, 3}; // second triangle
+
+
+    // triangle 1 vao
     glGenBuffers(1, &Triangle_VBO);
-    glGenBuffers(1, &Triangle_EBO);
+    glBindBuffer(GL_ARRAY_BUFFER, Triangle_VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+    // glGenBuffers(1, &Triangle_EBO);
 
     glGenVertexArrays(1, &Triangle_VAO);
     glBindVertexArray(Triangle_VAO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Triangle_EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Triangle_EBO);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ARRAY_BUFFER, Triangle_VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void *)0);
+    
     glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    // triangle 2 vao
+    glGenBuffers(1,&Triangle2_VBO);
+    glBindBuffer(GL_ARRAY_BUFFER,Triangle2_VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(verticiesT2),verticiesT2,GL_STATIC_DRAW);
+    
+    glGenVertexArrays(1,&Triangle2_VAO);
+    glBindVertexArray(Triangle2_VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,Triangle2_VBO);
+
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(float)*3,(void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
 }
 
 void initVShader()
@@ -147,12 +187,15 @@ void initVShader()
     }
 }
 
+
+
 void initFShader()
 {
     const char *fragmentShaderSource = "#version 330 core\n"
                                        "out vec4 color;\n"
                                        "void main(){\n"
                                        " color = vec4(0.2f,0.6f,0.7f,1.0f);\n"
+
                                        "}";
 
     fShaderID = glCreateShader(GL_FRAGMENT_SHADER);
@@ -168,6 +211,27 @@ void initFShader()
     }
 }
 
+void initFShader2()
+{
+    const char *fragmentShaderSource = "#version 330 core\n"
+                                       "out vec4 color;\n"
+                                       "void main(){\n"
+                                       " color = vec4(1.0f,1.0f,0.0f,1.0f);\n"
+
+                                       "}";
+
+    fShader2ID = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fShader2ID, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fShader2ID);
+    int status;
+    char logInfo[512];
+    glGetShaderiv(fShader2ID, GL_COMPILE_STATUS, &status);
+    if (!status)
+    {
+        glGetShaderInfoLog(fShader2ID, 512, NULL, logInfo);
+        std::cerr << "Failed to compile fragment shader\n " << logInfo << std::endl;
+    }
+}
 void createShaderProg()
 {
     shaderProg = glCreateProgram();
@@ -180,6 +244,23 @@ void createShaderProg()
     if (!status)
     {
         glGetProgramInfoLog(shaderProg, 512, NULL, logInfo);
+        std::cerr << "Failed to link shader program\n " << logInfo << std::endl;
+    }
+}
+
+
+void createShaderProg2()
+{
+    shaderProg2 = glCreateProgram();
+    glAttachShader(shaderProg2, vShaderID);
+    glAttachShader(shaderProg2, fShader2ID);
+    glLinkProgram(shaderProg2);
+    int status;
+    char logInfo[512];
+    glGetProgramiv(shaderProg2, GL_LINK_STATUS, &status);
+    if (!status)
+    {
+        glGetProgramInfoLog(shaderProg2, 512, NULL, logInfo);
         std::cerr << "Failed to link shader program\n " << logInfo << std::endl;
     }
 }
